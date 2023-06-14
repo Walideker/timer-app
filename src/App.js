afdcfaa02c11a0dd446bbd693e6d8c41500fdbc8
walideker <walidadr95@gmail.com>
2023-06-14 22:28:48 +0100

    frst commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './App.css'
 
-function App() {
+
+
+function Timer() {
+  const [time, setTime] = useState(0);
+  const [name, setName] = useState('start')
+  useEffect(() => {
+    return () => clearInterval(id.current)
+  }, [])
+  let id = useRef
+  function handleTime() {
+    id.current = setInterval(() => {
+      setTime((prev) => prev + 1)
+    }, 1000)
+    setName('continue')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+      <div className="container  ">
+        <nav className="navbar navbar-expend">
+          <div className="navbar-brand">
+            <i className="bi bi-stopwatch timer"></i>
+          </div>
+        </nav>
+        <div className="mt-5">
+          <h1 className="pt-5 ">{time}</h1>
+          <button className="btn btn-success " onClick={() => handleTime()}>{name}</button>
+          <button className="btn btn-success m-2 " onClick={() => clearInterval(id.current)}>pause</button>
+          <button className="btn btn-success" onClick={() => {
+            clearInterval(id.current)
+            setTime(0);
+            setName('start')
+          }}>reset</button>
+        </div>
+      </div>
+    </center>
   );
 }
 
-export default App;
+export default Timer;
